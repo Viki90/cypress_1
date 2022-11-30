048eb6d10e04b6147a5bfc74df38de846e14e9f9
Viki <viki.verebes90@gmail.com>
2022-11-30 20:05:11 +0100

    catch input id login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="Cypress" />
 
 describe("login test", () => {
-    it("visit gallery app", () => {
+    it("login with valid credentials", () => {
         cy.visit("https://gallery-app.vivifyideas.com/");
         // cy.get('a[href="/login"]');
         cy.get('a[class="nav-link nav-buttons"]').first().click();
+        cy.get('input[id="email"]');
     });
 });
