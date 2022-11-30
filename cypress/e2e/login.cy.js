1d7df68c53dc6910629b3647461519fb4962eb56
Viki <viki.verebes90@gmail.com>
2022-11-30 19:59:48 +0100

    redirected to login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,6 @@ describe("login test", () => {
     it("visit gallery app", () => {
         cy.visit("https://gallery-app.vivifyideas.com/");
         // cy.get('a[href="/login"]');
-        cy.get('a[class="nav-link nav-buttons"]').first();
+        cy.get('a[class="nav-link nav-buttons"]').first().click();
     });
 });
