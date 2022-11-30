40c7fc2802c51cfa740f66dabe7b49fddb25d527
Viki <viki.verebes90@gmail.com>
2022-11-30 21:43:06 +0100

    register

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,9 +1,30 @@
 /// <reference types="Cypress" />
 
 describe("registration test", () => {
+    function makeId(length) {
+        var result = "";
+        var characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+          result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+          );
+        }
+        return result;
+    }
+
+    let email = `${makeId(5)}@test.com`;
+
     it("register with valid input", () => {
         cy.visit("/");
         cy.get("a[href='/register']").click();
         cy.get("#first-name").type("Viki");
+        cy.get("#last-name").type("Developer");
+        cy.get("#email").type(email);
+        cy.get("#password").type("12341234");
+        cy.get("#password-confirmation").type("12341234");
+        cy.get('input[type="checkbox"]').check();
+        cy.get("button").click();
     });
 });
