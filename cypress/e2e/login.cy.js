f5529ae739afa95d16d9e8f1bf1bcf03c1533b87
Viki <viki.verebes90@gmail.com>
2022-11-30 19:55:17 +0100

    added function to find login button

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,5 +3,7 @@
 describe("login test", () => {
     it("visit gallery app", () => {
         cy.visit("https://gallery-app.vivifyideas.com/");
+        // cy.get('a[href="/login"]');
+        cy.get('a[class="nav-link nav-buttons"]').first();
     });
 });
