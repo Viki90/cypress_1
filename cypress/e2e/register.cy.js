6c285bdd56b7b73a444be955eeb80799f2f0c743
Viki <viki.verebes90@gmail.com>
2022-12-02 20:44:18 +0100

    faker

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,33 +1,71 @@
 /// <reference types="Cypress" />
 
+import { faker } from "@faker-js/faker";
 const Locators = require("../fixtures/Locators.json");
 
 describe("registration test", () => {
-    function makeId(length) {
-        var result = "";
-        var characters =
-          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-          result += characters.charAt(
-            Math.floor(Math.random() * charactersLength)
-          );
-        }
-        return result;
-    }
+  let randomUser = {
+    randomEmail: faker.internet.email(),
+    randomFirstName:faker.name.firstName(),
+    randomLastName: faker.name.lastName(),
+    randomPassword: faker.internet.password()
+  }
 
-    let email = `${makeId(5)}@test.com`;
+  beforeEach("visit gallery app", () => {
+    console.log("Random Email", randomEmail);
+    cy.visit("/");
+    cy.get("a[href='/register']").click();
+  });
 
-    it("register with valid input", () => {
-        cy.visit("/");
-        cy.get(Locators.Register.registerLink).click();
-        cy.get(Locators.Register.firstNameInput).type("Viki");
-        cy.get(Locators.Register.lastNameInput).type("Developer");
-        cy.get(Locators.SharedElements.emailInput).type(email);
-        cy.get(Locators.SharedElements.passwordInput).type("12341234");
-        cy.get(Locators.Register.passwordConfirmation).type("12341234");
-        cy.get(Locators.Register.checkbox).check();
-        cy.get(Locators.SharedElements.submitButton).click();
-        cy.url().should("not.include", "./register");
-    });
+  it("register without email address", () => {
+    cy.get("#first-name").type("Filip");
+    cy.get("#last-name").type("Nedovic");
+    cy.get("#password").type("Qwerty123!");
+    cy.get("#password-confirmation").type("Qwerty123!");
+    cy.get("input[type='checkbox']").check();
+    cy.get("button").click();
+    cy.url().should("include", "/register");
+  });
+
+  it("register without password ", () => {
+    cy.get("#first-name").type("Filip");
+    cy.get("#last-name").type("Nedovic");
+    cy.get("#email").type(email);
+    cy.get("#password-confirmation").type("Qwerty123!");
+    cy.get("input[type='checkbox']").check();
+    cy.get("button").click();
+    cy.url().should("include", "/register");
+  });
+
+  it("register without password confirmation", () => {
+    cy.get("#first-name").type("Filip");
+    cy.get("#last-name").type("Nedovic");
+    cy.get("#email").type(email);
+    cy.get("#password").type("Qwerty123!");
+    cy.get("input[type='checkbox']").check();
+    cy.get("button").click();
+    cy.url().should("include", "/register");
+  });
+
+  it("register without checking TOS", () => {
+    cy.get("#first-name").type("Filip");
+    cy.get("#last-name").type("Nedovic");
+    cy.get("#email").type(email);
+    cy.get("#password").type("Qwerty123!");
+    cy.get("#password-confirmation").type("Qwerty123!");
+    cy.get("button").click();
+    cy.url().should("include", "/register");
+  });
+
+  it.only("register with valid data", () => {
+    cy.get(Locators.Register.firstNameInput).type(randomFirstName);
+    cy.get(Locators.Register.lastNameInput).type(randomLastName);
+    cy.get(Locators.SharedElements.emailInput).type(randomEmail);
+    cy.get(Locators.SharedElements.passwordInput).type(randomPassword);
+    cy.get(Locators.Register.passwordConfirmation).type(randomPassword);
+    cy.get(Locators.Register.checkbox).check();
+    cy.get(Locators.SharedElements.submitButton).click();
+
+    cy.url().should("not.include", "/register");
+  });
 });
